284b338bdcdbdb0bbf1915b73abf52f41400afdd
admin <dangtinhtong@gmail.com>
2017-11-12 00:40:06 +0700

    first commit

--- Pro2.3/main.m
+++ Pro2.3/main.m
@@ -6,12 +6,19 @@
 //  Copyright © 2017 admin. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h> // This is a system file
+                                  // #import says to import or include the information from that file into the program, exactly as if the contents of the file were typed into program at that point
+                                  // imported the file Foundation.h becaus it has information about other classes or functions that are used later in the program
 
-int main(int argc, const char * argv[]) {
+
+int main(int argc, const char * argv[])  // Main is a special name that indicates precisely where the program is to begin excution
+ {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        NSLog(@"Tessting ....\n..1\n....2\n.....n\2");
+        
+        // The backslash (\) . The latter n are known collectively as the newline character
     }
     return 0;
 }
---
+++ Readme.md
@@ -0,0 +1,2 @@
+* This is a small program that use the backslash (\n) for newline
+* Use .gitignore : Để loại bỏ files tạm
